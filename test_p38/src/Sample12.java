1918d6e7763cd05d849bf9d6772ba880e1530be9
kkkhsu <kevin89100575@gmail.com>
2021-10-18 21:00:11 +0800

    edit

---
+++ test_p38/src/Sample12.java
@@ -0,0 +1,17 @@
+
+public class Sample12 {
+
+	public static void main(String[] args) {
+		double drum = 160.5;
+		
+		System.out.println("身高是" +drum +"公分");
+
+		System.out.println("指定給int型態的變數");
+		
+		int inum = (int) drum;
+		
+		System.out.println("身高是" +inum +"公分");
+		
+	}
+
+}
